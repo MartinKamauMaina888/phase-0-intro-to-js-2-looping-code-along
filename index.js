6f1705d676cdcec97811d066997fd05cbc851020
MartinKamauMaina888 <martin.maina01@student.moringaschool.com>
2022-11-29 12:42:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+// counting numbers downwards from 30 to 0
+// students to go through all the tests needed
+//'returns an array of thank you messages for each name provided to the function',
+//'invokes console.log once for each number, counting down from the number provided to zero'
+//'logs each number when counting down, starting from the number provided'
+
+function countDown(start_number)
+{
+
+    for (let num=start_number; num>=0;num--)
+
+    {
+    
+      console.log(`Number ${num}`);
+    
+    }
+}
+    
+    countDown(30)
